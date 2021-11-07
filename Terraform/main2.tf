15b222fd9c8492a7b44040758b23295dcf80e893
PodvolskihIlya <i89045462130@gmail.com>
2021-11-07 23:10:25 +0500

    7.4.

---
+++ Terraform/main2.tf
@@ -0,0 +1,77 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = ""
+  secret_key = ""
+}
+
+locals {
+  web_instance_type_map = {
+    stage = "t3.micro"
+    prod = "t3.large"
+  }
+}
+
+locals {
+  web_instance_count_map = {
+    stage = 1
+    prod = 2
+  }
+}
+
+locals {
+  instances = {
+    "t3.micro" = data.aws_ami.ubuntu.id   
+    "t3.large" = data.aws_ami.ubuntu.id
+  }
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Name = "var.role.var.project-var.environment"
+    Environment = "var.environment"
+    Project = "var.project"
+    Role = "var.role"
+    ForgeBucket = "telusdigital-forge"
+    ForgeRegion = "eu-central-1"
+    AmazonInspectorScan = "yes"
+  }
+  lifecycle {
+    create_before_destroy = true   
+    prevent_destroy = true
+    ignore_changes = ["tags"] 
+  }
+}
+
+data "aws_ami" "ubuntu" {
+
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    owners = ["099720109477"]
+}
+
+
+data "aws_caller_identity" "current" {}
+
+data "aws_regions" "current" {}
